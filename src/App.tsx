8bc0ab1f758d56486807480a6a14041d9e722193
Alex Pereira <33883321+AlexXevrPereira@users.noreply.github.com>
2021-07-24 23:39:00 +0800

    add education history (#2)
    Co-authored-by: Alex Pereira <alexevrpereira@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { FC } from 'react';
-import Header from './components/Header';
 import Resume from './components/Resume';
 import { Box } from '@material-ui/core';
 
 const App: FC = () => {
   return (
     <Box height="100vh">
-      <Header />
       <Resume />
     </Box>
   );
--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -1,15 +1,17 @@
 import React, { FC } from 'react';
 import { Grid, TextField } from '@material-ui/core';
 import PersonalDetails from './PersonalDetails';
+import ProfessionalSummary from './ProfessionalSummary';
+import EmploymentHistory from './EmploymentHistory';
 
 const Resume: FC = () => {
   return (
-    <Grid container spacing={2} padding={4}>
-      <Grid item xs={12}>
+    <Grid container padding={4}>
+      <Grid item xs={12} lg={6}>
         <TextField label="Resume Title" />
-      </Grid>
-      <Grid item xs={12}>
         <PersonalDetails />
+        <ProfessionalSummary />
+        <EmploymentHistory />
       </Grid>
     </Grid>
   );
